4d86eb4a85cd09d63a1337c44e4b42f0d4bbc98f
Akps42 <akshatshukla2244@gmail.com>
2025-11-24 21:58:04 +0530

    Update firebase.js
    api keys were removed

--- firebase.js
+++ firebase.js
@@ -4,13 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDDJfaTwMaRjKf6XzcLXV4n9tpQo8I-640",
-  authDomain: "drivo-36ada.firebaseapp.com",
-  projectId: "drivo-36ada",
-  storageBucket: "drivo-36ada.firebasestorage.app",
-  messagingSenderId: "544558033330",
-  appId: "1:544558033330:web:402e50408472ddcfe2dcab",
-  measurementId: "G-QCY98S62J3"
+  
 };
 
 // Initialize Firebase App
